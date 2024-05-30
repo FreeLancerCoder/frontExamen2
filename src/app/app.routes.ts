17d09ce262d1f90e7e8e51833d0effe095adb215
MilanLv <prodiyi1204@gmail.com>
2024-05-29 23:49:48 -0400

    sprint1Ex

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
+
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        loadComponent:()=>import('./login/login.component')
+    },
+    {
+        
+        path:'',
+        loadComponent:()=>import('./shered/components/layout/layout.component'),
+        children:[
+            
+            {
+                path:'dashboard',
+                loadComponent:()=>import('./business/dashboard/dashboard.component')
+            },
+            {
+                path:'profile',
+                loadComponent:()=>import('./business/profile/profile.component')
+            },
+            {
+                path:'tables',
+                loadComponent:()=>import('./business/tables/tables.component')
+            },
+        ]
+    },
+    {
+        path:'**',
+        redirectTo:'dashboard'
+    }
+];
